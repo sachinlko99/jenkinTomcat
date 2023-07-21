b00a303dfe1b6483c6c00578007a097b18b0b65e
sachin <sachin.lko99@gmail.com>
2023-07-21 12:44:11 +0530

    changes

---
+++ src/main/java/com/sachin/jenkinTomcat/JenkinTomcatApplication.java
@@ -0,0 +1,13 @@
+package com.sachin.jenkinTomcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinTomcatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinTomcatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sachin/jenkinTomcat/JenkinTomcatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sachin.jenkinTomcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinTomcatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
